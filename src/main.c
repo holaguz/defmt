306194cba7d98cf95453219afe9e3593f4b068f7
Agustin Catellani <agustin.catellani@gmail.com>
2024-04-06 19:19:35 -0300

    feat:  add streaming output to test the decoder

--- src/main.c
+++ src/main.c
@@ -1,13 +1,20 @@
 #include <stdlib.h>
 #include "defmt.h"
-#include <stdarg.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(void) {
   int a = 0x12345678, b = 0xABCDEF12;
   char c = 69;
 
-  defmt_printf("defmt_2_ints %d %d", a, b);
-  defmt_printf("defmt_1_char %c", c);
+  defmt_printf("defmt_2_ints %d %d\n", a, b);
+  defmt_printf("defmt_1_char %c\n", c);
+
+  while(1) {
+    time_t secs = time(NULL);
+    defmt_printf("time: %d\n", secs);
+    sleep(1);
+  }
 
   return 0;
 }
